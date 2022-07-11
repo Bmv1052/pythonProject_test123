ea51799fab5df2557f71ca79e3fe9e2763d00bd0
1024bill <william.4.jiang@gmail.com>
2022-07-10 18:44:51 -0700

    commint_5

--- 1024bill_0/pytest.py
+++ 1024bill_0/pytest.py
@@ -1,2 +1,6 @@
 import pymongo
 print('hello world' + '   7-10-2022')
+print('hello world' + '   7-10-2022_1')
+print('hello world' + '   7-10-2022_2')
+print('hello world' + '   VIP')
+print('hello world' + '   VIP2')
